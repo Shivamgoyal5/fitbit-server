30ea393a4b63f34ca1821a2dbb123511dfd13058
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-03 00:05:37 +0530

    Update login.jsx

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CLIENT_ID = "23QCJS";
-const REDIRECT_URI = "https://localhost:3000/callback";  
+const REDIRECT_URI = "https://fitbit-app-f95o.vercel.app/callback";  
 const FITBIT_AUTH_URL = `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800`;
 
 function Login() {
