c7341f25e5079d821b7fe3114a6938705b365e2b
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-05 23:02:47 +0530

    Update callback.js

--- client/src/callback.js
+++ client/src/callback.js
@@ -1,3 +1,33 @@
+// import React, { useEffect } from "react";
+// import { useNavigate, useSearchParams } from "react-router-dom";
+// import axios from "axios";
+
+// function Callback() {
+//     const navigate = useNavigate();
+//     const [searchParams] = useSearchParams();
+    
+//     useEffect(() => {
+//         const code = searchParams.get("code");
+
+//         if (code) {
+//             axios.get(`https://fitbit-app-backend.vercel.app/callback?code=${code}` , {
+//                 withCredentials: true
+//             })
+//             .then(response => {
+//                 localStorage.setItem("user_id", response.data.user_id);
+//                 navigate("/profile");
+//             })
+//             .catch(error => {
+//                 console.error("Error exchanging code for token", error);
+//             });
+//         }
+//     }, [searchParams, navigate]);
+
+//     return <h2>Processing Fitbit login...</h2>;
+// }
+
+// export default Callback;
+
 import React, { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
@@ -10,7 +40,9 @@ function Callback() {
         const code = searchParams.get("code");
 
         if (code) {
-            axios.get(`https://fitbit-app-backend.vercel.app/callback?code=${code}` , {
+            const url = `https://fitbit-app-backend.vercel.app/callback?code=${code}`;
+
+            axios.get(url, {
                 withCredentials: true
             })
             .then(response => {
