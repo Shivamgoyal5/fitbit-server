92112d874cb92ef891e0260db149404e395b2a9b
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-05 10:58:18 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -276,6 +276,10 @@ app.get("/profile", async (req, res) => {
 
 app.listen(process.env.PORT || 5000, () => console.log("Server running on http://localhost:5000"));
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 
 
