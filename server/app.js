6f744b08c09d402e6a17256db9e4b2e87dd21142
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-08 00:31:30 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -691,7 +691,7 @@ const session = require("express-session");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "https://fitbit-app-pwpk.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://fitbit-app-frontend.vercel.app", credentials: true }));
 app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
 
 const CLIENT_ID = "23QCJS";
