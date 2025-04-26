dcb9f0d85995b951d3918f0edce7908f7ba5f3d3
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-26 23:35:43 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -92,7 +92,7 @@ app.get("/profile", async (req, res) => {
         ]);
         console.log(profileRes.data);
         const name=profileRes.data.user.fullName;
-        const distance = caloriesRes.data.summary.calories?.distances?.find(d => d.activity === "total")
+        const walking = caloriesRes.data.summary.calories?.distances?.find(d => d.activity === "total")
         // const steps = await stepsRes.json();
         // const step = steps['activities-steps'][0].value;
         // const name = 
@@ -117,7 +117,7 @@ app.get("/profile", async (req, res) => {
       gender,
         steps,
       calories,
-      // walking,
+      walking,
     });
         
 
