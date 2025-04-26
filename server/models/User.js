2857779b01b2ab9bd7e9b3b9925ca82749c515df
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-26 20:19:41 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -12,53 +12,53 @@ const userInfo = new mongoose.Schema(
             required:true,
             maxLength:50,
         },
-        height: {
-            type: Number, 
-            min: 30, 
-            max: 300, 
+        // height: {
+        //     type: Number, 
+        //     min: 30, 
+        //     max: 300, 
+        //   },
+        //   weight: {
+        //     type: Number, 
+        //     min: 1, 
+        //     max: 500, 
+        //   },
+        //   age: {
+        //     type: Number, 
+        //     min: 0, 
+        //     max: 150, 
           },
-          weight: {
-            type: Number, 
-            min: 1, 
-            max: 500, 
-          },
-          age: {
-            type: Number, 
-            min: 0, 
-            max: 150, 
-          },
-          gender: {
-            type: String,
-            enum: ["Male", "Female", "Other"], 
-          },
-          walletAddress: {
-            type: String,
-            maxLength: 100, 
-          },
-          group: {
-            type: String,
-            enum: ["a", "b", "c",'d','e','f','g'], 
-          },
+          // gender: {
+          //   type: String,
+          //   enum: ["Male", "Female", "Other"], 
+          // },
+          // walletAddress: {
+          //   type: String,
+          //   maxLength: 100, 
+          // },
+          // group: {
+          //   type: String,
+          //   enum: ["a", "b", "c",'d','e','f','g'], 
+          // },
           steps: {
             type: Number, 
             min: 0,  
           },
-          calories: {
-            type: Number, 
-            min: 0,  
-          },
-          walking: {
-            type: Number, 
-            min: 0,  
-          },
-          running: {
-            type: Number, 
-            min: 0,  
-          },
-          cycling: {
-            type: Number, 
-            min: 0,  
-          },
+          // calories: {
+          //   type: Number, 
+          //   min: 0,  
+          // },
+          // walking: {
+          //   type: Number, 
+          //   min: 0,  
+          // },
+          // running: {
+          //   type: Number, 
+          //   min: 0,  
+          // },
+          // cycling: {
+          //   type: Number, 
+          //   min: 0,  
+          // },
     }
 );
 
