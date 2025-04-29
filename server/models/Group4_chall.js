790a18e4d3adc857772bb37b21fa77c1c2d1306e
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-29 23:57:12 +0530

    Update Group4_chall.js

--- server/models/Group4_chall.js
+++ server/models/Group4_chall.js
@@ -1,28 +1,43 @@
 const mongoose = require("mongoose");
 
-const Group4_chall = new mongoose.Schema(
-    {
-         name: [{
-            type: String,
-            maxLength: 50,
-        }],
-        email: {
-            type:String,
-            maxLength:50,
-        },
-        admin:{
-            type:String,
-        },   
-        Challenge:{
-            type:String,
-        } ,  
-        Quote:{
-            type:String,
-        }, 
-        Tips:{
-            type:String,
-        },  
-    }
-);
+const Group4_challSchema = new mongoose.Schema({
+  
+  
 
-module.exports = mongoose.model("Group4_chall", Group4_chall);
+  Challenge: {
+    steps: [String], // Array of step descriptions
+    caloriesBurned: {
+      type: Number,
+      default: 0,
+    },
+    caloriesRunning: {
+      type: Number,
+      default: 0,
+    },
+    caloriesCycling: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  Quote: {
+    type: String,
+  },
+
+  Tips: {
+    waterIntake: {
+      type: Number, // in liters
+    },
+    calorieIntake: {
+      type: Number,
+    },
+    sleepHours: {
+      type: Number,
+    },
+    foodRecommendation: {
+      type: String,
+    },
+  },
+});
+
+module.exports = mongoose.model("Group4_chall", Group4_challSchema);
