0a096534d6a86e0569975234e938c4e3730948a6
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-30 23:17:58 +0530

    Update Group4.js

--- server/models/Group4.js
+++ server/models/Group4.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
 
+const Group5 = new mongoose.Schema(
+    {
+         name: {
+            type: String,
+            maxLength: 50,
+         },
+         point:{
+            type:Number,
+         }
+       
+           
+        
+    }
+);
+
+module.exports = mongoose.model("Group5", Group5);
