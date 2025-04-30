1f95f08167e3ad9f648f228df1690d908f95567d
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-30 22:41:14 +0530

    Update Group1_chall.js

--- server/models/Group1_chall.js
+++ server/models/Group1_chall.js
@@ -5,7 +5,7 @@ const Group1_challSchema = new mongoose.Schema({
   
 
   Challenge: {
-    steps: [String], // Array of step descriptions
+    steps: Number, // Array of step descriptions
     caloriesBurned: {
       type: Number,
       default: 0,
