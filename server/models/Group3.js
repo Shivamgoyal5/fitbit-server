54c125dcd2a634fdece16f2d34ee034a3846c8f6
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-30 23:18:20 +0530

    Update Group3.js

--- server/models/Group3.js
+++ server/models/Group3.js
@@ -2,26 +2,16 @@ const mongoose = require("mongoose");
 
 const Group3 = new mongoose.Schema(
     {
-         name: [{
+         name: {
             type: String,
             maxLength: 50,
-        }],
-        email: {
-            type:String,
-            maxLength:50,
-        },
-        admin:{
-            type:String,
-        },   
-        Challenge:{
-            type:String,
-        } ,  
-        Quote:{
-            type:String,
-        }, 
-        Tips:{
-            type:String,
-        },  
+         },
+         point:{
+            type:Number,
+         },
+       
+           
+        
     }
 );
 
