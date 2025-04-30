4302685b4ac0de01382000e631e1760f6cd21826
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-30 23:00:38 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -298,48 +298,48 @@ if (groupModel) {
 
         
 
-// let user = await User.findOne({ name });
+let user = await User.findOne({ name });
 
-// if (user) {
-//   // Update only activity-related fields
-//   user.steps = steps;
-//   user.calories = calories;
-//   user.walking = walking;
-//   user.running = running;
-//   user.cycling = cycling;
-//   await user.save();
-// } else {
-//   // Create a new user
-//   user = await User.create({
-//     name,
-//     age,
-//     height,
-//     weight,
-//     gender,
-//     steps,
-//     calories,
-//     walking,
-//     running,
-//     cycling,
-//     group,
-//   });
-// }
+if (user) {
+  user.steps = steps;
+  user.calories = calories;
+  user.walking = walking;
+  user.running = running;
+  user.cycling = cycling;
+  await user.save();
+} else {
+  user = new User({
+    name,
+    age,
+    height,
+    weight,
+    gender,
+    steps,
+    calories,
+    walking,
+    running,
+    cycling,
+    group
+  });
+  await user.save();
+}
 
 
- const user = await User.create({
-      name,
-     // email,
-      age,
-      height,
-      weight,
-      gender,
-        steps,
-      calories,
-      walking,
-     running,
-     cycling,
-     group,
-    });
+
+ // const user = await User.create({
+ //      name,
+ //     // email,
+ //      age,
+ //      height,
+ //      weight,
+ //      gender,
+ //        steps,
+ //      calories,
+ //      walking,
+ //     running,
+ //     cycling,
+ //     group,
+ //    });     
         
 
 
