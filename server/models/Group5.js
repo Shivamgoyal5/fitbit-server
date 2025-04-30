7a9c3102be3377a775a4d71aae8fe2b0b67c6a71
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-04-30 23:17:05 +0530

    Update Group5.js

--- server/models/Group5.js
+++ server/models/Group5.js
@@ -1,91 +1,18 @@
-// const mongoose = require("mongoose");
-
-// const Group5 = new mongoose.Schema(
-//     {
-//          name: [{
-//             type: String,
-//             maxLength: 50,
-//         }],
-//         email: {
-//             type:String,
-//             maxLength:50,
-//         },
-//         admin:{
-//             type:String,
-//         },   
-//         Challenge:{
-//             type:String,
-//         } ,  
-//         Quote:{
-//             type:String,
-//         }, 
-//         Tips:{
-//             type:String,
-//         },  
-//     }
-// );
-
-// module.exports = mongoose.model("Group5", Group5);
-
-
-
-
-
-
-
-
-
 const mongoose = require("mongoose");
 
-const Group5Schema = new mongoose.Schema({
-  
-  name: [{
-    type: String,
-    maxLength: 50,
-  }],
-  email: {
-    type: String,
-    maxLength: 50,
-  },
-  admin: {
-    type: String,
-  },
+const Group5 = new mongoose.Schema(
+    {
+         name: {
+            type: String,
+            maxLength: 50,
+         },
+         point:{
+            type:Number,
+         }
+       
+           
+        
+    }
+);
 
-  Challenge: {
-    steps: [String], // Array of step descriptions
-    caloriesBurned: {
-      type: Number,
-      default: 0,
-    },
-    caloriesRunning: {
-      type: Number,
-      default: 0,
-    },
-    caloriesCycling: {
-      type: Number,
-      default: 0,
-    },
-  },
-
-  Quote: {
-    type: String,
-  },
-
-  Tips: {
-    waterIntake: {
-      type: Number, // in liters
-    },
-    calorieIntake: {
-      type: Number,
-    },
-    sleepHours: {
-      type: Number,
-    },
-    foodRecommendation: {
-      type: String,
-    },
-  },
-});
-
-module.exports = mongoose.model("Group5", Group5Schema);
-
+module.exports = mongoose.model("Group5", Group5);
